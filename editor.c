c6bd3e0bd34a9ada603cebe257db2231d2ed6302
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2018-05-06 15:10:07 +0530

    display key press and disable the following
    Ctrl-C Ctrl-Z Ctrl-S Ctrl-Q Ctrl-V and fix Ctrl-M

--- editor.c
+++ editor.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <stdio.h>
+#include <ctype.h>
 
 struct termios originalTermi;
 
@@ -14,7 +15,8 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = originalTermi;
-    raw.c_lflag = raw.c_lflag & (~(ECHO | ICANON));
+    raw.c_iflag = raw.c_iflag & (~(ICRNL | IXON));
+    raw.c_lflag = raw.c_lflag & (~(ECHO | ICANON | IEXTEN | ISIG));
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,6 +25,12 @@ int main() {
     enableRawMode();
 
     char ch;
-    while (read(STDIN_FILENO, &ch, 1) == 1 && !(ch == 'q' || ch == 'Q'));
+    while (read(STDIN_FILENO, &ch, 1) == 1 && !(ch == 'q' || ch == 'Q')) {
+        if (iscntrl(ch)) {
+            printf("%d\n", ch);
+        } else {
+            printf("%d ('%c')\n", ch, ch);
+        }
+    }
     return 0;
 }
