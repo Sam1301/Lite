371bd7b14a5e61908b6c27c9c93c1cfce0377c89
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2018-05-06 14:30:45 +0530

    disable raw mode on exit

--- editor.c
+++ editor.c
@@ -1,11 +1,19 @@
-#include <unistd.h>
+#include <stdlib.h>
 #include <termios.h>
+#include <unistd.h>
+
+
+struct termios originalTermi;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTermi);
+}
 
 void enableRawMode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &originalTermi);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
-
+    struct termios raw = originalTermi;
     raw.c_lflag = raw.c_lflag & (~ECHO);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -13,7 +21,7 @@ void enableRawMode() {
 
 int main() {
     enableRawMode();
-    
+
     char ch;
     while (read(STDIN_FILENO, &ch, 1) == 1 && !(ch == 'q' || ch == 'Q'));
     return 0;
