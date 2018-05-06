cdaed9ebbd94480985c7406ded1fb71dad9e855a
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2018-05-06 14:23:58 +0530

    turn off echo mode for terminal

--- editor.c
+++ editor.c
@@ -1,6 +1,19 @@
 #include <unistd.h>
+#include <termios.h>
+
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag = raw.c_lflag & (~ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    enableRawMode();
+    
     char ch;
     while (read(STDIN_FILENO, &ch, 1) == 1 && !(ch == 'q' || ch == 'Q'));
     return 0;
