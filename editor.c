ba0256b4b6ee5e65c7ed51571e4d1fe7f0d7c59d
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2018-05-28 13:06:19 +0530

    DEL Key with no func.

--- editor.c
+++ editor.c
@@ -22,7 +22,8 @@ enum editorKey {
     PAGE_UP,
     PAGE_DOWN,
     HOME_KEY,
-    END_KEY
+    END_KEY,
+    DEL_KEY
 };
 
 /*** data ***/
@@ -101,25 +102,21 @@ int editorReadKey() {
 
     if (ch == '\x1b') {
         char seq[3];
-        while (read(STDIN_FILENO, &seq[0], 1) != 1) {
-            return '\x1b';
-        }
-        while (read(STDIN_FILENO, &seq[1], 1) != 1) {
-            return '\x1b';
-        }
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
 
         if (seq[0] == '[') {
             if (seq[1] >= '0' && seq[1] <= '9') {
-                while (read(STDIN_FILENO, &seq[2], 1) != 1) {
-                    return '\x1b';
-                }
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
-                        case '1': return HOME_KEY;
                         case '7': return HOME_KEY;
-                        case '4': return END_KEY;
                         case '8': return END_KEY;
                     }
                 }       
